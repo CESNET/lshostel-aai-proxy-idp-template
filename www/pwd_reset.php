e857dff0c80c54152d8dc2442197455bb31db231
Pavel Vyskocil <Pavel.Vyskocil@cesnet.cz>
2021-03-18 15:27:42 +0100

    Prepare new version of theme 'lshostel2'

--- www/pwd_reset.php
+++ www/pwd_reset.php
@@ -7,35 +7,46 @@
 use SimpleSAML\Configuration;
 use SimpleSAML\Error\Exception;
 use SimpleSAML\Logger;
+use SimpleSAML\Module\perun\AdapterRpc;
 use SimpleSAML\XHTML\Template;
 
+const LANG_EN = 'en';
+const PWD_RESET = 'pwd_reset';
+const PWDRESET_LSHOSTEL_ENTITY_ID = 'lshostel_entity_id';
+const PWDRESET_LSHOSTEL_SCOPE = 'lshostel_scope';
+const PWDRESET_VO_SHORT_NAME = 'vo_short_name';
+const PWDRESET_PERUN_NAMESPACE = 'perun_namespace';
+const PWDRESET_PERUN_URL = 'perun_url';
+const PWDRESET_PERUN_EMAIL_ATTR = 'perun_email_attr';
 
-const LSHOSTEL_ENTITY_ID = "https://login.bbmri-eric.eu/lshostel/";
-const LSHOSTEL_SCOPE = "@lifescience-hostel.org";
-const VO_SHORT_NAME = "lifescience_hostel";
-const PERUN_NAMESPACE = "lifescience-hostel";
-const PERUN_URL = "https://perun.bbmri-eric.eu/";
-const EMAIL_ATTR_URN = "urn:perun:user:attribute-def:def:preferredMail";
-const LANG_EN = "en";
+const CONFIG_FILE_NAME = 'module_lshostel.php';
 
 $config = Configuration::getInstance();
-
 
 $t = new Template($config, 'lshostel:pwd_reset-tpl.php');
 $t->show();
 
 
 function sendPasswordResetEmail($userName) {
-    $rpcAdapter = new sspmod_perun_AdapterRpc();
+    $rpcAdapter = new AdapterRpc();
+
+    $conf = Configuration::getConfig(CONFIG_FILE_NAME);
+    $pwdConf = $conf->getConfigItem(PWD_RESET);
+    $lsHostelEntityId = $pwdConf->getString(PWDRESET_LSHOSTEL_ENTITY_ID);
+    $lsHostelScope = $pwdConf->getString(PWDRESET_LSHOSTEL_SCOPE);
+    $voShortName = $pwdConf->getString(PWDRESET_VO_SHORT_NAME);
+    $perunNamespace = $pwdConf->getString(PWDRESET_PERUN_NAMESPACE);
+    $perunUrl = $pwdConf->getString(PWDRESET_PERUN_URL);
+    $emailAttr = $pwdConf->getString(PWDRESET_PERUN_EMAIL_ATTR);
 
     $userName = trim($userName);
     $userNameParts = explode('@', $userName, 2);
     $userName = $userNameParts[0] . '_' . strtolower($userNameParts[1]);
 
-    $uid = [$userName . LSHOSTEL_SCOPE];
+    $uid = [$userName . $lsHostelScope];
     $user = null;
     try {
-        $user = $rpcAdapter->getPerunUser(LSHOSTEL_ENTITY_ID, $uid);
+        $user = $rpcAdapter->getPerunUser($lsHostelEntityId, $uid);
     } catch (Exception $ex) {
         throw new Exception("There are no LifeScience Hostel user with username: " . $userName, $ex);
     }
@@ -43,7 +54,7 @@ function sendPasswordResetEmail($userName) {
         throw new Exception("There are no LifeScience Hostel user with username: " . $userName);
     }
 
-    $vo = $rpcAdapter->getVoByShortName(VO_SHORT_NAME);
+    $vo = $rpcAdapter->getVoByShortName($voShortName);
     $member = $rpcAdapter->getMemberByUser($user, $vo);
 
     $connector = $rpcAdapter->getConnector();
@@ -52,9 +63,9 @@ function sendPasswordResetEmail($userName) {
         'sendPasswordResetLinkEmail',
         [
             'member' => $member->getId(),
-            'namespace' => PERUN_NAMESPACE,
-            'url' => PERUN_URL,
-            'emailAttributeURN' => EMAIL_ATTR_URN,
+            'namespace' => $perunNamespace,
+            'url' => $perunUrl,
+            'emailAttributeURN' => $emailAttr,
             'language' => LANG_EN
         ]);
 
